1018a2ca0e296b63e19d80853eb1335a13a82695
caeamer <shangguanmoge@gmail.com>
2026-07-08 23:48:15 +0800

    Bump to v1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.watchdog"
         minSdk = 24
         targetSdk = 37
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
